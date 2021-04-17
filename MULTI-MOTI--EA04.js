99e3f30804ea938aa19c1ab5369a3f4dd8dfd64e
wedone <wedone@users.noreply.github.com>
2021-04-18 01:04:27 +0800

    Add files via upload

---
+++ MULTI-MOTI--EA04.js
@@ -0,0 +1,20 @@
+const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
+const tz = require('zigbee-herdsman-converters/converters/toZigbee');
+const exposes = require('zigbee-herdsman-converters/lib/exposes');
+const reporting = require('zigbee-herdsman-converters/lib/reporting');
+const e = exposes.presets;
+const ea = exposes.access;
+
+const device = {
+    zigbeeModel: ['MULTI-MOTI--EA04'],
+    model: 'MULTI-MOTI--EA04',
+    vendor: 'Shuncom',
+    icon: 'img/magnet.png',
+    description: 'human body movement sensor',
+    fromZigbee: [fz.ias_occupancy_alarm_1_with_timeout, fz.battery],
+    toZigbee: [],
+    exposes: [e.occupancy(), e.battery_low(), e.tamper()],
+    meta: {battery: {voltageToPercentage: '3V_2500'}},
+};
+
+module.exports = device;
