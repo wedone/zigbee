fb9794e710aa7acf00e577004a1eba70db437c9f
wedone <wedone@users.noreply.github.com>
2021-12-23 21:52:13 +0800

    整理目录

---
+++ 亿慧云开关/YHY-S4.js
@@ -0,0 +1,38 @@
+const zigbeeHerdsmanConverters = require('zigbee-herdsman-converters');
+
+const exposes = zigbeeHerdsmanConverters.exposes;
+const ea = exposes.access;
+const e = exposes.presets;
+const fz = zigbeeHerdsmanConverters.fromZigbeeConverters;
+const tz = zigbeeHerdsmanConverters.toZigbeeConverters;
+
+const ptvo_switch = zigbeeHerdsmanConverters.findByDevice({modelID: 'ptvo.switch'});
+fz.legacy = ptvo_switch.meta.tuyaThermostatPreset;
+
+const device = {
+    zigbeeModel: ['YHY-S4'],
+    model: 'YHY-S4',
+    vendor: 'YiHuiYun',
+    description: 'Yihuiyun quadruple key wired wall light switch',
+    fromZigbee: [fz.ignore_basic_report, fz.on_off, fz.ptvo_multistate_action, fz.legacy.ptvo_switch_buttons,],
+    toZigbee: [tz.ptvo_switch_trigger, tz.on_off,],
+    exposes: [e.switch().withEndpoint('l2'),
+      e.switch().withEndpoint('l3'),
+      e.switch().withEndpoint('l4'),
+      e.switch().withEndpoint('l5'),
+      e.action(['single', 'double', 'triple', 'hold', 'release']),
+      e.linkquality(),
+],
+    meta: {
+        multiEndpoint: true,
+        
+    },
+    endpoint: (device) => {
+        return {
+            l2: 2, l3: 3, l4: 4, l5: 5, l1: 1,
+        };
+    },
+    
+};
+
+module.exports = device;
